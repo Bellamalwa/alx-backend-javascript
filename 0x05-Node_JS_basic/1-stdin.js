fa8f921b812c1ba2f0314f4271e29ebc637898d1
Bellamalwa <duchybella@gmail.com>
2024-03-18 14:43:51 -0700

    bug fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 // Gets input from user and writes it to stdout
 process.stdin.setEncoding('utf-8'); // set encoding to utf-8
 
-process.stdout.write('Welcome to ALX School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read(); // read input from stdin
   if (name) process.stdout.write(`Your name is: ${name}`);
