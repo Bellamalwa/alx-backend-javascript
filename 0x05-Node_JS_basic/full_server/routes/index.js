0649f089723e3f940d6f4b0afeca5f877a75ee9b
Bellamalwa <duchybella@gmail.com>
2024-03-18 14:38:52 -0700

    JSBASICS

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,20 @@
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+/**
+ * Bind all routes to the app instance passed in as parameter and export the router.
+ * @param {Express} app - Express application instance.
+ * @author Christabel <https://github.com/Bellamalwa>
+ */
+
+const mapRoutes = (app) => {
+  // Homepage route (GET) - /
+  app.get('/', AppController.getHomepage);
+  // Get all students (GET) - /students
+  app.get('/students', StudentsController.getAllStudents);
+  // Get all students by major (GET) - /students/:major (e.g. /students/CS)
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
+
+export default mapRoutes; // Export the router to be used in the server.js file.
+module.exports = mapRoutes;
